4c615ab16fe42b1195f70b53ce93d14de7ecf5f7
fsenycouty <fabrice.seny-couty@oclock.school>
2026-08-28 13:17:04 +0200

    feat: connexion PostgreSQL et modèles Sequelize avec associations

---
+++ api/src/models/User.js
@@ -0,0 +1,28 @@
+import sequelize from "../config/sequelize.client.js";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+    },
+
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+    },
+
+    password_hash: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+  },
+
+  { tableName: "user" },
+);
+
+export default User;
