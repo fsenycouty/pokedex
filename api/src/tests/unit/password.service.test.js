e3c35acc3b9924cc3dade074423d86ec318c5032
fsenycouty <fabrice.seny-couty@oclock.school>
2026-09-10 15:54:40 +0200

    test github action

--- api/src/tests/unit/password.service.test.js
+++ api/src/tests/unit/password.service.test.js
@@ -27,6 +27,6 @@ describe("password.service", () => {
 
     const result = await verifyPassword(hash, "MauvaisMotDePasse1");
 
-    assert.strictEqual(result, true);
+    assert.strictEqual(result, false);
   });
 });
