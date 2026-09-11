65d5786992d72927e35a859245d3cceff4e8a5b5
fsenycouty <fabrice.seny-couty@oclock.school>
2026-09-11 23:29:21 +0200

    fix: corrige la référence server non déclarée dans gracefulShutdown

--- api/src/server.js
+++ api/src/server.js
@@ -4,7 +4,7 @@ import sequelize from "./config/sequelize.client.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
